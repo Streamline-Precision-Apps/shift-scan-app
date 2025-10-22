98bf286e114e84fff5d294e913180dd72440a659
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-10-22 11:08:02 -0600

    save bundling and building of template for next 16 capacitor

---
+++ shift-scan/capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.shiftscanapp",
+  appName: "Shift Scan",
+  webDir: "out",
+  server: {
+    url: "http://192.168.1.102:3000",
+    cleartext: true,
+  },
+};
+
+export default config;
